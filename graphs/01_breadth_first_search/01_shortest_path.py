3c13635b12b30190eb52767cec5a41b4bc09ba98
ja-odur <odurjoseph8@gmail.com>
2024-10-14 16:30:43 +0300

    BFS - shortest path grid

--- graphs/01_breadth_first_search/01_shortest_path.py
+++ graphs/01_breadth_first_search/01_shortest_path.py
@@ -28,6 +28,73 @@ def shortest_path(graph, node, target):
                 queue.append(neighbor)
 
 
+# Dungeon Problem Statement
+# You are trapped in a 2D dungeon and need to find the quickest way out! The dungeon is composed of unit
+# cubes which may or may not be filled with rock. It takes one minute to move one unit north, south,
+# east, west. You cannot move diagonally and the maze is surrounded by solid rock on all sides.
+# Is an escape possible? If yes, how long will it take?
+def shortest_path_dungeon(grid, r_start, c_start, r_end, c_end, rock="#"):
+    row_count = len(grid)
+    col_count = len(grid[0])
+
+    def key(row, col):
+        return f"({row}, {col})"
+
+    def get_neighbors(row, col):
+        dr = [-1, +1, 0, 0]
+        dc = [0, 0, +1, -1]
+        neighbors = []
+
+        for i in range(4):
+            rr = row + dr[i]
+            cc = col + dc[i]
+
+            if rr < 0 or cc < 0: continue;
+            if rr >= row_count or cc >= col_count: continue;
+
+            if grid[rr][cc] == rock: continue;
+
+            neighbors.append((rr, cc))
+
+        return neighbors
+
+    visited = {}
+    r_queue = deque()
+    c_queue = deque()
+
+    visited[key(r_start, c_start)] = None
+
+    r_queue.append(r_start)
+    c_queue.append(c_start)
+
+    while r_queue:
+        current_row = r_queue.popleft()
+        current_col = c_queue.popleft()
+        print("processing node - ", current_row, current_col)
+
+        if current_row == r_end and current_col == c_end:
+            path = []
+            current_node = key(current_row, current_col)
+
+            while current_node:
+                path.append(current_node)
+                current_node = visited[current_node]
+
+            return path[::-1]
+
+        for r, c in get_neighbors(current_row, current_col):
+            if key(r, c) not in visited:
+                visited[key(r, c)] = key(current_row, current_col)
+                r_queue.append(r)
+                c_queue.append(c)
+
+def get_grid_coordinates(grid, target):
+    for row, value in enumerate(grid):
+        for col, col_value in enumerate(value):
+            if col_value == target:
+                return row, col
+    return None, None
+
 
 if __name__ == '__main__':
     graph = {
@@ -45,4 +112,18 @@ if __name__ == '__main__':
     }
 
     path = shortest_path(graph, 'S', 'J')
+    print(path)
+
+    grid = [
+        "S..#...",
+        ".#...#.",
+        ".#.....",
+        "..##...",
+        "#.#E.#."
+    ]
+
+    r_start, c_start = get_grid_coordinates(grid, 'S')
+    r_end, c_end = get_grid_coordinates(grid, 'E')
+
+    path = shortest_path_dungeon(grid, r_start, c_start, r_end, c_end)
     print(path)
